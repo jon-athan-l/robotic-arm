57fd906e88f0fe44b9ed2fe8398baaced7fb2aa6
jon-athan-l <jbl@berkeley.edu>
2020-12-07 07:06:41 -0800

    added keyboard-theta.py, a manual control script
    Control the arm by setting explicit position thetas. Needs to run with the appropriate microcontroller firmware.

---
+++ keyboard-theta.py
@@ -0,0 +1,16 @@
+import serial
+import time
+
+port = serial.Serial('/dev/cu.usbmodem14201', 9600, timeout=1)
+print("-----------------------------------------------------")
+print("Communications online on " + port.name)
+print()
+print("Enter swivel steps (<400), tilt steps (<90), slide steps (???)")
+
+
+while True:
+    instruction = input(">>>   ")
+    # to have a distinct terminating character for the parsing on the firmware to recognize
+    instruction = instruction + "!"
+    port.write(instruction.encode())
+    time.sleep(0.001)
